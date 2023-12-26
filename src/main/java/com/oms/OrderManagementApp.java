66e1cf2ce986a252425ad6151e58a12dc7f596a1
Pranavmobi <pranav@mobiuso.com>
2023-12-26 15:13:57 +0530

    initial commit

---
+++ src/main/java/com/oms/OrderManagementApp.java
@@ -0,0 +1,14 @@
+package com.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OrderManagementApp.class,args);
+	}
+
+}
